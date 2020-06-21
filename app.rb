d91d4478af71a702fbe4084d8012cbb979e326ee
Toni  Diep <adiep123@gmail.com>
2020-06-21 20:25:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -8,17 +8,18 @@ class App < Sinatra::Base
 	end
   
   
-  get '/' do
-      erb :index
-  end
+  # get '/' do
+  #     erb :index
+  # end
   
-  get "/info" do
-    "Testing the info page"
-  end
+  # get "/info" do
+  #   "Testing the info page"
+  # end
 
   get "/info" do
     "This is the info page:"
      erb :info
   end
+
 
 end
